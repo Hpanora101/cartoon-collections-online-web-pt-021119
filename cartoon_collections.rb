bad10c0c89ef4a0c5d4181da64aef4541cab3d8f
Henry Panora <hpanora@gmail.com>
2019-03-02 04:26:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+  require "pry"
+
+  def roll_call_dwarves(dwarves)
+    dwarves.each.with_index(1) do |name, index|
+      puts "/#{index}.*#{name}/"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+  require "pry"
+  def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map do |element|
+      element = element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+  require "pry"
+  def long_planeteer_calls (words)
+    words.any? do |word|
+      word.length > 4
+    end
+
 end
 
 def find_the_cheese# code an argument here
